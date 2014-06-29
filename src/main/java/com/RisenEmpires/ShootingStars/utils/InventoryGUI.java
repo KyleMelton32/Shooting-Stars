ad08b51184da70b9ce2a1baf49284486a7404f3a
Mammothskier <fs2dwyab@gmail.com>
2014-06-28 22:24:14 -0700

    Update to inventoryGUI manager

--- src/main/java/com/RisenEmpires/ShootingStars/utils/InventoryGUI.java
+++ src/main/java/com/RisenEmpires/ShootingStars/utils/InventoryGUI.java
@@ -1,5 +1,8 @@
 package com.RisenEmpires.ShootingStars.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -17,13 +20,19 @@ public class InventoryGUI implements Listener {
 			case "Commands":
 				Inventory inventory = Bukkit.createInventory(null, 9, ChatColor.AQUA + "Shooting Star Commands");
 				
-				ItemStack spawn = new ItemStack (Material.DIAMOND_CHESTPLATE);
-				ItemMeta spawnMeta = spawn.getItemMeta();
-				
-				spawnMeta.setDisplayName(ChatColor.GREEN + "Spawn");
-				spawn.setItemMeta(spawnMeta);
-				
-				inventory.setItem(5, spawn);
+				if ((player.hasPermission("shootingstars.spawn") || player.hasPermission("shootingstars.*") || player.isOp())) {
+					ItemStack spawn = new ItemStack (Material.NETHER_STAR);
+					ItemMeta spawnMeta = spawn.getItemMeta();
+					List<String> spawnLore = Arrays.asList(ChatColor.UNDERLINE + "" + ChatColor.ITALIC + "Shooting Stars Spawn",
+							ChatColor.RESET + "Spawns a star in 3 minutes at your location");
+					
+					spawnMeta.setDisplayName(ChatColor.GREEN + "Spawn");
+
+					spawnMeta.setLore(spawnLore);
+					spawn.setItemMeta(spawnMeta);
+					
+					inventory.setItem(5, spawn);
+				}
 				
 				player.openInventory(inventory);
 				break;
