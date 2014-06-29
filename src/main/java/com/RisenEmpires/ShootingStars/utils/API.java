d525608257a441ed14aad3f9e94cb0003e46b9a2
Mammothskier <fs2dwyab@gmail.com>
2014-06-28 20:39:45 -0700

    Adding API

--- src/main/java/com/RisenEmpires/ShootingStars/utils/API.java
+++ src/main/java/com/RisenEmpires/ShootingStars/utils/API.java
@@ -1,11 +1,26 @@
 package com.RisenEmpires.ShootingStars.utils;
 
 import com.RisenEmpires.ShootingStars.ShootingStars;
+import com.RisenEmpires.ShootingStars.events.GUIListeners;
+import com.RisenEmpires.ShootingStars.files.FileHandler;
 
 public class API {
-
+	private static ShootingStars _stars;
+	private static FileHandler fileHandler;
+	private static InventoryGUI inventoryGUI;
+	
 	public API(ShootingStars shootingStars) {
-		// TODO Auto-generated constructor stub
+		_stars = shootingStars;
+		fileHandler = new FileHandler(_stars);
+		new InventoryGUI();
+		new GUIListeners();
 	}
-
+	
+	public static FileHandler getFileHandler() {
+		return fileHandler;
+	}
+	
+	public static InventoryGUI getInventoryGUI() {
+		return inventoryGUI;
+	}
 }
